a4830de49d1c732576d3fccb608a022d41662b68
DrghamDev <drgham.dev@gmail.com>
2023-04-25 15:42:28 +0300

    Edit Mongodb URI

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+        const connection = await mongoose.connect("mongodb+srv://drghamdev:SDt6cIV9k2JJBtCL@cluster0.nkrgt1o.mongodb.net/test");
         console.log(`MongoDB connected ${connection.connection.host}`);
     } catch (error) {
         console.log(error);
