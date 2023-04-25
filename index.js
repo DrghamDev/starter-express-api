2a6024c39151c44816c3a46588525eff08f4dfb7
DrghamDev <drgham.dev@gmail.com>
2023-04-25 14:39:46 +0300

    Add default get route

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message : "Welcome to my app",
+    })
+})
 app.listen(process.env.PORT || 3000)
