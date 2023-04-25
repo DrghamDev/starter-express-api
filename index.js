f63235d3e792cd75171535795a4db32366d9943a
DrghamDev <drgham.dev@gmail.com>
2023-04-25 14:42:06 +0300

    Edit all route

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
 
 app.get('/', (req, res) => {
     res.status(200).json({
         message : "Welcome to my app",
     })
 })
+
 app.listen(process.env.PORT || 3000)
